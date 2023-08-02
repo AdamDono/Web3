89fb33d0b6b490d79b3bcb258a313bb71717b5cb
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-08-02 10:27:09 +0200

    connected buttons to the view controller and added cocoa touch

---
+++ Web3/Loginview Controller.swift
@@ -0,0 +1,29 @@
+//
+//  Loginview Controller.swift
+//  Web3
+//
+//  Created by DA M1 MAC 89 on 2023/08/02.
+//
+
+import UIKit
+
+class Loginview_Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
